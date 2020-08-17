a98991a0e5ffdfb7db1ff5384b06a35c11c85b34
KarolJash <karojl6309@gmail.com>
2020-08-17 14:00:22 -0400

    Updates

---
+++ precache-manifest.d4b87042f2a7058bf6f7684a6fb0a593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62782ddbc54412abf41ef7d31ad7f6d3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6b7dca73849afd8ddb50",
+    "url": "/monsters-rolodex/static/css/main.9f6c3e2e.chunk.css"
+  },
+  {
+    "revision": "bc4f0aec8753b31eee9d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7dca73849afd8ddb50",
+    "url": "/monsters-rolodex/static/js/main.ed20befd.chunk.js"
+  },
+  {
+    "revision": "99aeed3ea4362ed85205",
+    "url": "/monsters-rolodex/static/js/runtime-main.dcaacdb6.js"
+  }
+]);
